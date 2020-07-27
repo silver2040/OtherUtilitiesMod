d53d73cff2272576ce0bc7856d9db036ac25f241
rayyan1819 <rayyan1819@gmail.com>
2020-07-27 18:13:40 -0400

    one way glass and insanity block

--- src/main/java/com/Silver2040/OtherUtilities/objects/blocks/InsanityBlock.java
+++ src/main/java/com/Silver2040/OtherUtilities/objects/blocks/InsanityBlock.java
@@ -3,17 +3,21 @@ package com.Silver2040.OtherUtilities.objects.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.Items;
 import net.minecraftforge.common.ToolType;
 
-public class CaliberOreBlock extends Block {
-    public CaliberOreBlock() {
+public class InsanityBlock extends Block{
+    public InsanityBlock() {
         super(Block.Properties.create(Material.IRON)
                 .hardnessAndResistance(3.0f,9.0f)
                 .sound(SoundType.METAL)
                 .harvestLevel(2)
                 .harvestTool(ToolType.PICKAXE)
-
-        );
+                .func_235838_a_((p_235464_0_) -> {
+                    return 5;
+                }));
 
     }
+
+
 }
